b58019d56127310592ae2a329870e872e31296db
AijiaLi <1354210286@qq.com>
2026-07-07 07:19:50 +0000

    [LeetCode Sync] Runtime - 4 ms (26.49%), Memory - 8.8 MB (65.05%)

--- solutions/0009-palindrome-number/solution.c
+++ solutions/0009-palindrome-number/solution.c
@@ -8,28 +8,26 @@ bool isPalindrome(int x) {
 
     // 计算数字的位数，并求出最高位对应的除数
     int digits = 1;//位数默认，至少有1位
-    long long divisor = 1;   // divisor = 10^(digits-1)
-    int temp = x;
-    while (temp >= 10) {
-        temp /= 10;
-        digits++;
-        divisor *= 10;
+    int divisor = 1;   // divisor除数先设为1 = 10^(digits-1) 用于选出最高位
+    int temp = x;  //写一个x的临时变量
+    while (temp >= 10) { //被除成个位之前
+        temp /= 10;  //除十的同时用整数除法把小数省略，达到消除最后一位的效果
+        digits++;  //每循环一次，位数加一
+        divisor *= 10; //除数变为10的n次方，用于除掉十进制数字每一位上的值
     }
 
-    int num = x;
-
     // 比较 digits/2 的长度的首位数字
-    for (int i = 0; i < digits / 2; i++) {
-        int firstDigit = (num / divisor) % 10;  // 当前最高位
-        int lastDigit = num % 10;               // 当前最低位
+    for (int i = 0; i < digits / 2; i++) {  //总长的一半
+        int firstDigit = (x / divisor) % 10;  // 除divisor后只剩最高位，除十后的余数即当前最高位（相当于读取该值）
+        int lastDigit = x % 10;               // 原数除十后读取余数，即当前最低位
 
         if (firstDigit != lastDigit) {
             return false;
         }
 
         // 把已经比较过的数字排除
-        num = (num % divisor) / 10;  // 去掉最高位和最低位
-        divisor /= 100;               // 除数缩小两位，指向新的最高宽度
+        x = (x % divisor) / 10;  // 去掉最高位和最低位
+        divisor /= 100;  // 除数缩小两位（首尾在上一轮被取出了），指向新的最高宽度
     }
 
     return true;
